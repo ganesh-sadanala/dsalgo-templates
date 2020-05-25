44fd8243e29659bf29d016bd256239c0d1ed4810
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-05-25 11:32:47 +0530

    Add ps.java

---
+++ ps.java
@@ -0,0 +1,3 @@
+// https://www.hackerrank.com/contests/code-athon-final-round/challenges/prateek-and-his-safe
+// Solution:
+// https://mail.google.com/mail/u/0/#search/code+athon/FMfcgxwHNWDpWdRkRWQcpGpmnnvFNNWV
