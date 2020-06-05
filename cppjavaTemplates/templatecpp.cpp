8924b868c125216bbfa29367f41e33292f71afad
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-06-05 15:09:03 +0530

    Update templatecpp.cpp

--- cppjavaTemplates/templatecpp.cpp
+++ cppjavaTemplates/templatecpp.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 #define pb push_back
 #define vi vector<int>
-#define vpii vector<pair<int,int>>
-#define N 2e5+5
+#define vpii vector<pair<int, int>>
+#define N 2e5 + 5
 #define f first
 #define s second
-#define all(x) x.begin(),x.end()
-#define int long long int
-#define forn(i,n) for(int i=0;i<n;i++)
-#define fore(i,l,r) for(int i=l;i<r;i++)
+#define all(x) x.begin(), x.end()
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define fore(i, l, r) for (int i = l; i < r; i++)
 #define sz(a) (int)((a).size())
 #define ll long long
 #define ar array
-#define init(arr) memset(arr,0,sizeof(arr))
+#define init(arr) memset(arr, 0, sizeof(arr))
 
-int32_t main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	solve();
+    solve();
 }
