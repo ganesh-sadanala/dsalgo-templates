82379e8bdc4553f32b56d70eb125aef876f0458b
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-06-19 10:59:42 +0530

    Add coding pbms

--- cppjavaTemplates/templatecpp.cpp
+++ cppjavaTemplates/templatecpp.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 #define pb push_back
 #define vi vector<int>
-#define vpii vector<pair<int, int>>
 #define N 2e5 + 5
 #define f first
 #define s second
 #define all(x) x.begin(), x.end()
 #define forn(i, n) for (int i = 0; i < n; i++)
-#define fore(i, l, r) for (int i = l; i < r; i++)
-#define sz(a) (int)((a).size())
 #define ll long long
-#define ar array
 #define init(arr) memset(arr, 0, sizeof(arr))
 #define endl "\n"
 #define mp make_pair
