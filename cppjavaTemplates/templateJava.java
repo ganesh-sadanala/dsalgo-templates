f8ff9ba6c5a310b34c29b43e1a36beb7efceedc6
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2021-12-11 15:23:08 +0530

    Update template Java

--- cppjavaTemplates/templateJava.java
+++ cppjavaTemplates/templateJava.java
@@ -234,6 +234,29 @@ class Codechef {
         return res;
     }
   
+  // Function to count
+    // derangements
+    static int countDer(int n)
+    {
+        // Create an array to store
+        // counts for subproblems
+        int der[] = new int[n + 1];
+     
+        // Base cases
+        der[1] = 0;
+        der[2] = 1;
+     
+        // Fill der[0..n] in bottom up
+        // manner using above recursive
+        // formula
+        for (int i = 3; i <= n; ++i)
+            der[i] = (i - 1) * (der[i - 1] +
+                                der[i - 2]);
+     
+        // Return result for n
+        return der[n];
+    }
+  
   public static void main(String[] args) throws java.lang.Exception {
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     int t = Integer.parseInt(bf.readLine().trim());
