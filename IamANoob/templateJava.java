001949bd540e64d5009b543409b06180c07f32af
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-04-05 21:00:13 +0530

    Update templateJava.java

--- IamANoob/templateJava.java
+++ IamANoob/templateJava.java
@@ -33,13 +33,10 @@ https://codeforces.com/blog/entry/48122
 // https://oeis.org/ ->  The On-Line Encyclopedia of Integer Sequences -> If problem in contests are about sequences
 // Google Interview Experience : https://docs.google.com/document/d/15PwjWY5leHJCvwtNQi9aCH9EASTa6HPmiQWTKbEnJkU/edit
 // Not a Cycle of odd length or Bipartite: https://www.geeksforgeeks.org/check-graphs-cycle-odd-length/
-// Learning -> Solving -> Contesting -> Upsolving -> Learning
-// A Holiday an Algo, New Stuff
 // cp book -> https://github.com/luisfcofv/competitive-programming-book
 // https://www.hackerearth.com/practice/notes/getting-started-with-the-sport-of-programming/
-/* Motivation: https://www.youtube.com/watch?v=KNviwfDeRKg */
+// Motivation: https://www.youtube.com/watch?v=KNviwfDeRKg 
 // Whenever you don't able to solve, it means u did not solve closer idea problem -> so make a lot of observations.
-// +Key to Excel: Think more, Think all, Never Give Up!
 /*
 Interesting hidden things in java
 https://stackoverflow.com/questions/19485818/why-are-2-long-variables-not-equal-with-operator-in-java
@@ -221,6 +218,53 @@ Then the multiplicative identity holds i.e; d(a * b) = d(a) * d(b) if gcd(a, b)=
 
 a+b=(a^b)+2*(a&b)
 a+b=(a&b)+(a|b)
+38
+
+Bitwise operations that are just a boolean operator applied between corresponding bits of the operands follow laws analogous to the laws of Boolean algebra, for example:
+
+AND (&) : Commutative, Associative, Identity (0xFF), Annihilator (0x00), Idempotent
+OR  (|) : Commutative, Associative, Identity (0x00), Annihilator (0xFF), Idempotent
+XOR (^) : Commutative, Associative, Identity (0x00), Inverse (itself)
+NOT (~) : Inverse (itself)
+AND and OR absorb each other:
+
+a & (a | b) = a
+a | (a & b) = a
+There are some pairs of distributive operators, such as:
+
+AND over OR: a & (b | c) = (a & b) | (a & c)
+AND over XOR: a & (b ^ c) = (a & b) ^ (a & c)
+OR over AND: a | (b & c) = (a | b) & (a | c)
+Note however that XOR does not distribute over AND or OR, and neither does OR distribute over XOR.
+
+DeMorgans law applies in its various forms:
+
+~(a & b) = ~a | ~b
+~(a | b) = ~a & ~b
+Some laws that relate XOR and AND can be found by reasoning about the field ℤ/2ℤ, in which addition corresponds to XOR and multiplication to AND:
+
+AND distributes over XOR
+Working out products of sums: (a ^ b) & (c ^ d) = (a & c) ^ (a & d) ^ (b & c) ^ (b & d)
+There are some laws that combine arithmetic and bitwise operations:
+
+Subtract by adding: a - b = ~(~a + b)
+Add carries separately: a + b = (a ^ b) + ((a & b) << 1)
+Turn min into max and vice versa: min(a, b) = ~max(~a, ~b), max(a, b) = ~min(~a, ~b)
+Shifts have no inverse because of the "destruction" of bits pushed to the edge
+
+left shift  (<<) : Associative, Distributive, Identity (0x00)
+
+right shift (>>) : Associative, Distributive, Identity (0x00)
+
+rotate left  (rl) : Associative, Distributive, Identity (0x00), Inverse (rr)
+
+rotate right (rr) : Associative, Distributive, Identity (0x00), Inverse (rl)
+
+While shifts have no inverse, some expressions involving shifts do have inverses as consequence of other laws, for example:
+
+x + (x << k) has an inverse, because it is effectively a multiplication by an odd number and odd numbers have an modular multiplicative inverse modulo a power of two. For x + (x << 1) = x * 3, the inverse is x * 0xAAAAAAAB (for 32 bits, adjust the constant for other sizes)
+x ^ (x << k) has an inverse for a similar reason, but through the correspondence with carryless multiplication.
+Similarly x ^ (x >> k) (with unsigned right shift) has an inverse, it's just the "mirror image" of the above.
 
 (a^(b^c))%m != (a^((b^c)%m))%m -> https://codeforces.com/problemset/problem/1594/E1
 
@@ -304,15 +348,56 @@ A=B*Q+R
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 **//*
 cur -> CF unsolved
 Queue 1 -----------------https://codeforces.com/blog/entry/65487?#comment-494720
 https://codeforces.com/blog/entry/46450 : Div2B reasoning and comparison is great
-https://codeforces.com/contest/961/problem/C
-https://codeforces.com/contest/1395/problem/C
-https://codeforces.com/contest/812/problem/B
-https://codeforces.com/contest/244/problem/B
-https://codeforces.com/contest/243/problem/A
 https://codeforces.com/contest/1325/problem/D
 https://codeforces.com/contest/1559/problem/D2 // The key idea is after performing the 1st operation i.e; merging all possible nodes with 1 in both the graphs. Then nodes that are not connected in L with 1 are connected in R with 1. The vice versa i.e; nodes that are not connected in R with 1 are connected in L with 1.
 https://codeforces.com/contest/1560/problem/E
@@ -615,13 +700,17 @@ class Codechef {
 			return scale(1 / d);
 		}
 		
+		double abs(){
+			return Math.sqrt(norm2());
+		}
+		
 	}
 	
 	static class Line{
 		static final double INF = 1e9, EPS = 1e-9;
 		double a, b, c;
 		
-		// Vector v(-b, a) is parallel to the line represented
+		// Vector v(b, -a) is parallel to the line represented
 		// as two points p and q. pq vector is equal v vector
 		Line(Point p, Point q)
 		{
@@ -637,6 +726,23 @@ class Codechef {
 		Line(Point p, double m) { a = -m; b = 1; c =  -(a * p.x + p.y); } 
 		boolean parallel(Line l) { return Math.abs(a - l.a) < EPS && Math.abs(b - l.b) < EPS; }
 		boolean same(Line l) { return parallel(l) && Math.abs(c - l.c) < EPS; }
+		
+		// Point p on the side of line v(b, -a)
+		// 0 if p is on v
+		// <0 if p is on right of v
+		// >0 if p is on left of v
+		boolean side(Point p){
+			return v.cross(new Vector(p))-c;
+		}
+		double dist(Point p){
+			return Math.abs(side(p)/v.abs());
+		}
+		/*
+			The squared distance can be useful to check if a point is within a certain
+			integer distance of a line, because when using integers the result is exact if
+			it is an integer.
+			double sqDist(pt p) {return side(p)*side(p) / (double)sq(v);}
+		*/
 		
 	}
  
