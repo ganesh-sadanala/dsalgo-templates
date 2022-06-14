7df6b5c23b86201fc5cc9afdc07daac3c5559174
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-06-14 15:23:42 +0530

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -1433,7 +1433,7 @@ class Codechef {
         iterations++;
     }
 
-    // ----------------------------- #GRAPHS -------------------------
+    // ----------------------------- #GRAPHS #TREES-------------------------
     // Bi Partite graph: https://www.geeksforgeeks.org/bipartite-graph/
 	
     // count number of times each edge is counted in all simple paths
@@ -1671,6 +1671,121 @@ class Codechef {
         return res;
     }
 
+    class Node
+	{
+		int data;
+		Node left, right;
+
+		Node(int item)
+		{
+			data = item;
+			left = right = null;
+		}
+	}
+  
+	class BinarySearchTree
+	{
+		Node root;
+
+		/* Function to find LCA of n1 and n2. The function assumes that both
+		   n1 and n2 are present in BST */
+		Node lcaInBSTRecursive(Node node, int n1, int n2)
+		{
+			if (node == null)
+				return null;
+
+			// If both n1 and n2 are smaller than root, then LCA lies in left
+			if (node.data > n1 && node.data > n2)
+				return lca(node.left, n1, n2);
+
+			// If both n1 and n2 are greater than root, then LCA lies in right
+			if (node.data < n1 && node.data < n2)
+				return lca(node.right, n1, n2);
+
+			return node;
+		}
+		
+		static Node lcaInBSTIterative(Node root, int n1, int n2)
+		{
+			while (root != null)
+			{
+				// If both n1 and n2 are smaller
+				// than root, then LCA lies in left
+				if (root.data > n1 &&
+					root.data > n2)
+				root = root.left;
+
+				// If both n1 and n2 are greater
+				// than root, then LCA lies in right
+				else if (root.data < n1 &&
+						 root.data < n2)
+				root = root.right;
+
+				else break;
+			}
+			return root;
+		}
+	}
+	
+	Node findLCAUtilBT(Node node, int n1, int n2)
+    {
+        // Base case
+        if (node == null)
+            return null;
+         
+        //Store result in temp, in case of key match so that we can search for other key also.
+        Node temp=null;
+ 
+        // If either n1 or n2 matches with root's key, report the presence
+        // by setting v1 or v2 as true and return root (Note that if a key
+        // is ancestor of other, then the ancestor key becomes LCA)
+        if (node.data == n1)
+        {
+            v1 = true;
+            temp = node;
+        }
+        if (node.data == n2)
+        {
+            v2 = true;
+            temp = node;
+        }
+ 
+        // Look for keys in left and right subtrees
+        Node left_lca = findLCAUtil(node.left, n1, n2);
+        Node right_lca = findLCAUtil(node.right, n1, n2);
+ 
+        if (temp != null)
+            return temp;
+ 
+        // If both of the above calls return Non-NULL, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
+        if (left_lca != null && right_lca != null)
+            return node;
+ 
+        // Otherwise check if left subtree or right subtree is LCA
+        return (left_lca != null) ? left_lca : right_lca;
+    }
+ 
+    // Finds lca of n1 and n2 under the subtree rooted with 'node'
+    Node findLCABT(int n1, int n2)
+    {
+        // Initialize n1 and n2 as not visited
+        v1 = false;
+        v2 = false;
+ 
+        // Find lca of n1 and n2 using the technique discussed above
+        Node lca = findLCAUtil(root, n1, n2);
+ 
+        // Return LCA only if both n1 and n2 are present in tree
+        if (v1 && v2)
+            return lca;
+ 
+        // Else return NULL
+        return null;
+    }
+	
+	
     // ------------------------- #ADVANCED DATA STRUCTURES ------------------------
     // #BIT
     // Using 2 BIT structures => https://codeforces.com/contest/1311/problem/F
