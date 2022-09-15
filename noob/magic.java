f9a5d7002895701d10bb55a262736cc4855e4578
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-09-14 23:59:50 -0400

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -398,8 +398,6 @@ which can be obtained by 2*sqrt(M) * sqrt(M) = M where first 2*sqrt(M) is the nu
 **//*
 CF unsolved/upsolved/master
 ---------------------------
-https://codeforces.com/contest/1619/problem/D
-https://codeforces.com/contest/238/problem/A
 https://codeforces.com/contest/1620/problem/C
 https://codeforces.com/contest/1691/problem/C
 https://codeforces.com/contest/1342/problem/C
