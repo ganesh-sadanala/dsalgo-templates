649d42b08c9eba9c35f899b0929dfa4d4824827f
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-09-26 22:26:03 -0400

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -458,6 +458,7 @@ https://codeforces.com/contest/1557/problem/C
 https://codeforces.com/contest/1734/problem/D
 https://www.facebook.com/codingcompetitions/hacker-cup/2022/round-2/problems/A2 : https://codeforces.com/blog/entry/107302#comment-956427
 https://codeforces.com/contest/1730/problem/D
+https://codeforces.com/contest/1730/problem/D
 
 Solved and Learned other approaches / Took more time to solve / Got AC by luck
 ------------------------------------------------------------------------------
