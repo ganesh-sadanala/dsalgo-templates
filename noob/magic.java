b8d646456bfa6938e3563d50a5f4750e32e8126d
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-10-07 15:42:18 -0400

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -466,6 +466,7 @@ https://codeforces.com/problemset/problem/1426/F : https://www.youtube.com/watch
 https://codeforces.com/problemset/problem/1391/C : Editorial
 https://codeforces.com/problemset/problem/1238/D
 https://codeforces.com/contest/1737/problem/B : Math Proof https://www.youtube.com/watch?v=6bpLuSsK8L0
+https://codeforces.com/contest/1737/problem/A
 
 Solved and Learned other approaches / Took more time to solve / Got AC by luck
 ------------------------------------------------------------------------------
