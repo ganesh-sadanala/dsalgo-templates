4dbfb1e5b7f27537d096361585442a5676a1decf
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-10-19 22:08:33 -0400

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -506,7 +506,7 @@ https://codeforces.com/contest/1253/problem/D
 https://www.hackerrank.com/challenges/stone-division/problem
 https://www.hackerrank.com/challenges/tower-breakers-revisited-1/problem
 https://www.spoj.com/problems/GNY07H/
-- DP on graphs is enchanting 
+- DP on graphs is enchanting, Master Articulation Points and Bridges
 - #145 Edit Distance with operations can be performed on any of the strings == Edit Distance with operations can be performed on any one specific string.
 - #178, #179, #180, #181, #182, DP Bit masks problems Bottom Up, DP with combinatorics($90, #91, #93)
 https://codeforces.com/contest/1573/problem/C : failed at dp
@@ -2591,7 +2591,273 @@ class Codechef {
 	
 	// #FLOOD FILL ALGORITHM
 	
+	// Articulation Points and Bridges
+
+	class Graph {
+
+		static int time;
+
+		static void addEdge(ArrayList<ArrayList<Integer> > adj, int u, int v)
+		{
+			adj.get(u).add(v);
+			adj.get(v).add(u);
+		}
+
+		static void APUtil(ArrayList<ArrayList<Integer> > adj, int u,
+						boolean visited[], int disc[], int low[],
+						int parent, boolean isAP[])
+		{
+			// Count of children in DFS Tree
+			int children = 0;
+
+			// Mark the current node as visited
+			visited[u] = true;
+
+			// Initialize discovery time and low value
+			disc[u] = low[u] = ++time;
+
+			// Go through all vertices adjacent to this
+			for (Integer v : adj.get(u)) {
+				// If v is not visited yet, then make it a child of u
+				// in DFS tree and recur for it
+				if (!visited[v]) {
+					children++;
+					APUtil(adj, v, visited, disc, low, u, isAP);
+
+					// Check if the subtree rooted with v has
+					// a connection to one of the ancestors of u
+					low[u] = Math.min(low[u], low[v]);
+
+					// If u is not root and low value of one of
+					// its child is more than discovery value of u.
+					if (parent != -1 && low[v] >= disc[u])
+						isAP[u] = true;
+				}
+
+				// Update low value of u for parent function calls.
+				else if (v != parent)
+					low[u] = Math.min(low[u], disc[v]);
+			}
+
+			// If u is root of DFS tree and has two or more children.
+			if (parent == -1 && children > 1)
+				isAP[u] = true;
+		}
+
+		static void AP(ArrayList<ArrayList<Integer> > adj, int V)
+		{
+			boolean[] visited = new boolean[V];
+			int[] disc = new int[V];
+			int[] low = new int[V];
+			boolean[] isAP = new boolean[V];
+			int time = 0, par = -1;
+
+			// Adding this loop so that the
+			// code works even if we are given
+			// disconnected graph
+			for (int u = 0; u < V; u++)
+				if (visited[u] == false)
+					APUtil(adj, u, visited, disc, low, par, isAP);
+
+			for (int u = 0; u < V; u++)
+				if (isAP[u] == true)
+					System.out.print(u + " ");
+			System.out.println();
+		}
+
+		public static void main(String[] args)
+		{
+
+			// Creating first example graph
+			int V = 5;
+			ArrayList<ArrayList<Integer> > adj1 =
+							new ArrayList<ArrayList<Integer> >(V);
+			for (int i = 0; i < V; i++)
+				adj1.add(new ArrayList<Integer>());
+			addEdge(adj1, 1, 0);
+			addEdge(adj1, 0, 2);
+			addEdge(adj1, 2, 1);
+			addEdge(adj1, 0, 3);
+			addEdge(adj1, 3, 4);
+			System.out.println("Articulation points in first graph");
+			AP(adj1, V);
+
+			// Creating second example graph
+			V = 4;
+			ArrayList<ArrayList<Integer> > adj2 =
+							new ArrayList<ArrayList<Integer> >(V);
+			for (int i = 0; i < V; i++)
+				adj2.add(new ArrayList<Integer>());
+
+			addEdge(adj2, 0, 1);
+			addEdge(adj2, 1, 2);
+			addEdge(adj2, 2, 3);
+
+			System.out.println("Articulation points in second graph");
+			AP(adj2, V);
+
+			// Creating third example graph
+			V = 7;
+			ArrayList<ArrayList<Integer> > adj3 =
+								new ArrayList<ArrayList<Integer> >(V);
+			for (int i = 0; i < V; i++)
+				adj3.add(new ArrayList<Integer>());
+
+			addEdge(adj3, 0, 1);
+			addEdge(adj3, 1, 2);
+			addEdge(adj3, 2, 0);
+			addEdge(adj3, 1, 3);
+			addEdge(adj3, 1, 4);
+			addEdge(adj3, 1, 6);
+			addEdge(adj3, 3, 5);
+			addEdge(adj3, 4, 5);
+
+			System.out.println("Articulation points in third graph");
+
+			AP(adj3, V);
+		}
+	}
 	
+	// Bridges in Graph
+	import java.io.*;
+	import java.util.*;
+	import java.util.LinkedList;
+
+	// This class represents a undirected graph using adjacency list
+	// representation
+	class Graph
+	{
+		private int V; // No. of vertices
+
+		// Array of lists for Adjacency List Representation
+		private LinkedList<Integer> adj[];
+		int time = 0;
+		static final int NIL = -1;
+
+		// Constructor
+		@SuppressWarnings("unchecked")Graph(int v)
+		{
+			V = v;
+			adj = new LinkedList[v];
+			for (int i=0; i<v; ++i)
+				adj[i] = new LinkedList();
+		}
+
+		// Function to add an edge into the graph
+		void addEdge(int v, int w)
+		{
+			adj[v].add(w); // Add w to v's list.
+			adj[w].add(v); //Add v to w's list
+		}
+
+		// A recursive function that finds and prints bridges
+		// using DFS traversal
+		// u --> The vertex to be visited next
+		// visited[] --> keeps track of visited vertices
+		// disc[] --> Stores discovery times of visited vertices
+		// parent[] --> Stores parent vertices in DFS tree
+		void bridgeUtil(int u, boolean visited[], int disc[],
+						int low[], int parent[])
+		{
+
+			// Mark the current node as visited
+			visited[u] = true;
+
+			// Initialize discovery time and low value
+			disc[u] = low[u] = ++time;
+
+			// Go through all vertices adjacent to this
+			Iterator<Integer> i = adj[u].iterator();
+			while (i.hasNext())
+			{
+				int v = i.next(); // v is current adjacent of u
+
+				// If v is not visited yet, then make it a child
+				// of u in DFS tree and recur for it.
+				// If v is not visited yet, then recur for it
+				if (!visited[v])
+				{
+					parent[v] = u;
+					bridgeUtil(v, visited, disc, low, parent);
+
+					// Check if the subtree rooted with v has a
+					// connection to one of the ancestors of u
+					low[u] = Math.min(low[u], low[v]);
+
+					// If the lowest vertex reachable from subtree
+					// under v is below u in DFS tree, then u-v is
+					// a bridge
+					if (low[v] > disc[u])
+						System.out.println(u+" "+v);
+				}
+
+				// Update low value of u for parent function calls.
+				else if (v != parent[u])
+					low[u] = Math.min(low[u], disc[v]);
+			}
+		}
+
+
+		// DFS based function to find all bridges. It uses recursive
+		// function bridgeUtil()
+		void bridge()
+		{
+			// Mark all the vertices as not visited
+			boolean visited[] = new boolean[V];
+			int disc[] = new int[V];
+			int low[] = new int[V];
+			int parent[] = new int[V];
+
+
+			// Initialize parent and visited, and ap(articulation point)
+			// arrays
+			for (int i = 0; i < V; i++)
+			{
+				parent[i] = NIL;
+				visited[i] = false;
+			}
+
+			// Call the recursive helper function to find Bridges
+			// in DFS tree rooted with vertex 'i'
+			for (int i = 0; i < V; i++)
+				if (visited[i] == false)
+					bridgeUtil(i, visited, disc, low, parent);
+		}
+
+		public static void main(String args[])
+		{
+			// Create graphs given in above diagrams
+			System.out.println("Bridges in first graph ");
+			Graph g1 = new Graph(5);
+			g1.addEdge(1, 0);
+			g1.addEdge(0, 2);
+			g1.addEdge(2, 1);
+			g1.addEdge(0, 3);
+			g1.addEdge(3, 4);
+			g1.bridge();
+			System.out.println();
+
+			System.out.println("Bridges in Second graph");
+			Graph g2 = new Graph(4);
+			g2.addEdge(0, 1);
+			g2.addEdge(1, 2);
+			g2.addEdge(2, 3);
+			g2.bridge();
+			System.out.println();
+
+			System.out.println("Bridges in Third graph ");
+			Graph g3 = new Graph(7);
+			g3.addEdge(0, 1);
+			g3.addEdge(1, 2);
+			g3.addEdge(2, 0);
+			g3.addEdge(1, 3);
+			g3.addEdge(1, 4);
+			g3.addEdge(1, 6);
+			g3.addEdge(3, 5);
+			g3.addEdge(4, 5);
+			g3.bridge();
+		}
+	}
 	
     // ------------------------- #ADVANCED DATA STRUCTURES ------------------------
     // #BIT
