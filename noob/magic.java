6eed23e64cd5f2f1d81ae506cfbcda09166f0479
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2022-11-25 16:40:02 -0500

    Update magic.java

--- noob/magic.java
+++ noob/magic.java
@@ -577,6 +577,7 @@ https://cses.fi/problemset/task/1144
 https://www.spoj.com/problems/MKTHNUM/
 https://cses.fi/problemset/task/1144
 https://codeforces.com/contest/1758/problem/C
+https://codeforces.com/contest/1758/problem/D
 
 Solved and Learned other approaches / Took more time to solve / Got AC by luck
 ------------------------------------------------------------------------------
